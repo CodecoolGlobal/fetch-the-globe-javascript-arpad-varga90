91024e9ff1cdf6943d4a14c6132574e933404b9a
arpad.varga90 <arpad.varga90@gmail.com>
2024-05-15 11:08:25 +0200

    1st task solution

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello World</h1>
-`
+// GET All Countries
+// https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all
+
+// GET Countries by cca3
+// https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/by-cca3/FRA
+
+
+const app = document.querySelector<HTMLDivElement>("#app");
+
+if (app) {
+  app.innerHTML = `<h1>Hello World</h1>`;
+} else {
+  console.log("Error happened");
+}
