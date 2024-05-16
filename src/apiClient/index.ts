520fa901c95da493d915e68bd3b07b4daaba30e7
arpad.varga90 <arpad.varga90@gmail.com>
2024-05-16 10:44:45 +0200

    do reexport to clean code

---
+++ src/apiClient/index.ts
@@ -0,0 +1,7 @@
+import getCountries from "./getCountries";
+import getCountryDetails from "./getCountriDetails";
+
+import type { Country } from "./getCountries";
+import type { CountryDetails } from "./getCountriDetails";
+
+export { getCountries, getCountryDetails, Country, CountryDetails };
